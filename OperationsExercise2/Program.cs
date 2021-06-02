05c609219485e30267718df897360a613cea4d76
mattiyee <mattiyee610@gmail.com>
2021-06-02 10:36:25 -0400

    initial commit

---
+++ OperationsExercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OperationsExercise2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            double circleValue = Math.PI * radius * radius;
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {circleValue}.");
+
+        }
+    }
+}
