6633cc53a24f9d5617bf857a671dfce9ad8e5470
mattiyee <mattiyee610@gmail.com>
2021-06-02 13:39:06 -0400

    added method

--- OperationsExercise2/Program.cs
+++ OperationsExercise2/Program.cs
@@ -4,15 +4,19 @@ namespace OperationsExercise2
 {
     class Program
     {
+        public static double AreaOfCircle(int radius)
+        {
+            return Math.PI * (radius * radius);
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine("What is the radius of your circle?");
-            var radius = double.Parse(Console.ReadLine());
+            var radius = int.Parse(Console.ReadLine());
 
-            double circleValue = Math.PI * radius * radius;
+            var circleValue = AreaOfCircle(radius);
 
-            Console.WriteLine($"The area of a circle with radius of {radius} is {circleValue}.");
+            Console.WriteLine($"The area of a circle with a radius of {radius} is equal to {circleValue}.");
 
         }
     }
